acbedff34eab1ac46c648d2e7bd245a209d3887c
Imtiazkun <alshahriar.me@gmail.com>
2024-06-16 20:53:40 +0600

    Added Google Analytics to all pages

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,9 +1,11 @@
 import Footer from "@/components/navigation/footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col justify-between min-h-screen">
       {children} <Footer />
+      <GoogleAnalytics gaId="G-0Z4ZMMGBWT" />
     </div>
   );
 }
